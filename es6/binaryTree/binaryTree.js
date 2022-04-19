28acca2c1fc24b1f93e6859ba4e58c11e59d93b1
Entaro Adun <dcwmark@gmail.com>
2022-04-19 18:32:26 -0400

    Updates

---
+++ es6/binaryTree/binaryTree.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class Node {
+  constructor(val) {
+      this.key = val;
+      this.left = null;
+      this.right = null;
+  }
+}
+
+const binaryTree = () => {
+  let root = null;
+
+};
+
+module.export=binaryTree;
---
+++ es6/linkedlist/linkedlist.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const linkedlist = () => {
+	const node = {
+		prevNode: null,
+		currData: null,
+		nextNode: null,
+	};
+};
+
+module.export = linkedlist;
